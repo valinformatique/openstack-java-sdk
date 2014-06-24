ea1b1f1e4ff2f9b7d9c60f9479dd2a1ef9f33f14
valinformatique <github@valinformatique.fr>
2014-06-24 11:42:09 +0200

    add support for security groups, and other evolutions

--- nova-client/src/main/java/com/woorea/openstack/nova/api/extensions/VolumesExtension.java
+++ nova-client/src/main/java/com/woorea/openstack/nova/api/extensions/VolumesExtension.java
@@ -8,99 +8,105 @@ import com.woorea.openstack.nova.model.Metadata;
 import com.woorea.openstack.nova.model.Volume;
 import com.woorea.openstack.nova.model.VolumeForCreate;
 import com.woorea.openstack.nova.model.VolumeForImageCreate;
+import com.woorea.openstack.nova.model.VolumeQuotaDefaults;
 import com.woorea.openstack.nova.model.Volumes;
 
 public class VolumesExtension {
 
-	private final OpenStackClient CLIENT;
+    private final OpenStackClient CLIENT;
 
-	public VolumesExtension(OpenStackClient client) {
-		CLIENT = client;
-	}
+    public VolumesExtension(OpenStackClient client) {
+        CLIENT = client;
+    }
 
-	public List list(boolean detail) {
-		return new List(detail);
-	}
+    public List list(boolean detail) {
+        return new List(detail);
+    }
 
-	public Create create(VolumeForCreate volume) {
-		return new Create(volume);
-	}
+    public Create create(VolumeForCreate volume) {
+        return new Create(volume);
+    }
 
-	public UploadToImage uploadToImage(VolumeForImageCreate volumeForImage) {
-		return new UploadToImage(volumeForImage);
-	}
+    public UploadToImage uploadToImage(VolumeForImageCreate volumeForImage) {
+        return new UploadToImage(volumeForImage);
+    }
 
-	public Show show(String id) {
-		return new Show(id);
-	}
+    public Show show(String id) {
+        return new Show(id);
+    }
 
-	public ShowMetadata showMetadata(String id) {
-		return new ShowMetadata(id);
-	}
+    public ShowMetadata showMetadata(String id) {
+        return new ShowMetadata(id);
+    }
 
-	public Delete delete(String id) {
-		return new Delete(id);
-	}
+    public Delete delete(String id) {
+        return new Delete(id);
+    }
 
-	public class List extends OpenStackRequest<Volumes> {
+    public class List extends OpenStackRequest<Volumes> {
 
-		public List(boolean detail) {
-			super(CLIENT, HttpMethod.GET, detail ? "/os-volumes/detail"
-					: "/os-volumes", null, Volumes.class);
-		}
+        public List(boolean detail) {
+            super(CLIENT, HttpMethod.GET, detail ? "/os-volumes/detail"
+                    : "/os-volumes", null, Volumes.class);
+        }
+    }
 
-	}
+    public class Create extends OpenStackRequest<Volume> {
 
-	public class Create extends OpenStackRequest<Volume> {
+        // private Volume volume;
+        public Create(VolumeForCreate volume) {
+            super(CLIENT, HttpMethod.POST, "/os-volumes", Entity.json(volume),
+                    Volume.class);
+            // this.volume = volume;
+        }
+    }
 
-		// private Volume volume;
+    // Upload volume to image service as image
+    public class UploadToImage extends OpenStackRequest<Void> {
 
-		public Create(VolumeForCreate volume) {
-			super(CLIENT, HttpMethod.POST, "/os-volumes", Entity.json(volume),
-					Volume.class);
-			// this.volume = volume;
-		}
+        public UploadToImage(VolumeForImageCreate volumeForImageCreate) {
+            super(CLIENT, HttpMethod.POST, new StringBuilder("/volumes/").append(volumeForImageCreate.getVolumeId())
+                    .append("/action"),
+                    Entity.json(volumeForImageCreate), Void.class);
+        }
+    }
 
-	}
+    public class Show extends OpenStackRequest<Volume> {
 
-	// Upload volume to image service as image
+        public Show(String id) {
+            super(CLIENT, HttpMethod.GET, new StringBuilder("/os-volumes/")
+                    .append(id).toString(), null, Volume.class);
+        }
+    }
 
-	public class UploadToImage extends OpenStackRequest<Void> {
+    public class ShowMetadata extends OpenStackRequest<Metadata> {
 
-		public UploadToImage(VolumeForImageCreate volumeForImageCreate) {
-			super(CLIENT, HttpMethod.POST, new StringBuilder("/volumes/")
-					.append(volumeForImageCreate.getVolumeId() + "/action"),
-					Entity.json(volumeForImageCreate), Void.class);
-		}
+        public ShowMetadata(String id) {
+            super(CLIENT, HttpMethod.GET, new StringBuilder("/os-volumes/")
+                    .append(id).append("/metadata").toString(), null,
+                    Metadata.class);
+        }
+    }
 
-	}
+    public class Delete extends OpenStackRequest<Void> {
 
-	public class Show extends OpenStackRequest<Volume> {
+        public Delete(String id) {
+            super(CLIENT, HttpMethod.DELETE, new StringBuilder("/os-volumes/")
+                    .append(id).toString(), null, Void.class);
+        }
+    }
 
-		public Show(String id) {
-			super(CLIENT, HttpMethod.GET, new StringBuilder("/os-volumes/")
-					.append(id).toString(), null, Volume.class);
-		}
+    public QuotaDefaults getquotaDefaults(String id) {
+        return new QuotaDefaults(id);
+    }
 
-	}
+    public class QuotaDefaults extends OpenStackRequest<VolumeQuotaDefaults> {
 
-	public class ShowMetadata extends OpenStackRequest<Metadata> {
+        public QuotaDefaults(String id) {
 
-		public ShowMetadata(String id) {
-			super(CLIENT, HttpMethod.GET, new StringBuilder("/os-volumes/")
-					.append(id).append("/metadata").toString(), null,
-					Metadata.class);
-		}
-
-	}
-
-	public class Delete extends OpenStackRequest<Void> {
-
-		public Delete(String id) {
-			super(CLIENT, HttpMethod.DELETE, new StringBuilder("/os-volumes/")
-					.append(id).toString(), null, Void.class);
-		}
-
-	}
-
+            super(CLIENT, HttpMethod.GET, new StringBuilder("/os-quota-sets/")
+                    .append(id).append("/defaults"), null,
+                    VolumeQuotaDefaults.class);
+        }
+    }
 }
--- nova-model/src/main/java/com/woorea/openstack/nova/model/Metadata.java
+++ nova-model/src/main/java/com/woorea/openstack/nova/model/Metadata.java
@@ -1,26 +1,25 @@
 package com.woorea.openstack.nova.model;
 
+import java.io.Serializable;
 import java.util.Map;
 
-public class Metadata {
+public class Metadata implements Serializable {
 
-	private Map<String, String> metadata;
+    private Map<String, String> metadata;
 
-	/**
-	 * @return the metadata
-	 */
-	public Map<String, String> getMetadata() {
-		return metadata;
-	}
+    /**
+     * @return the metadata
+     */
+    public Map<String, String> getMetadata() {
+        return metadata;
+    }
 
-	/**
-	 * Set the metadata
-	 * @param metadata
-	 */
-	public void setMetadata(Map<String, String> metadata) {
-		this.metadata = metadata;
-	}
-	
-
-	
+    /**
+     * Set the metadata
+     *
+     * @param metadata
+     */
+    public void setMetadata(Map<String, String> metadata) {
+        this.metadata = metadata;
+    }
 }
